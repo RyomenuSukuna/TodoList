8d5f59fd6979c389280fd24123a7a9af1a587a9b
Swanand <swanandkulkarni1812@gmail.com>
2024-08-23 13:02:34 +0530

    third commit

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -10,7 +10,7 @@ export default function TodoInput(props) {
         onChange={(e) => {
           setTodoValue(e.target.value);
         }}
-        placeholder="Enter Todo..."
+        placeholder="Enter todo..."
       />
       <button
         onClick={() => {
